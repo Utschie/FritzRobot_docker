3d7d9af3acdfb93f2ec4b3fca0d416c5db9cda1d
Utschie <littlecat.j@hotmail.com>
2023-11-08 03:33:24 +0100

    modified Dockerfile to fit arm64

--- README.md
+++ README.md
@@ -13,7 +13,8 @@ stop the nodes
 cd ./docker
 docker-compose down
 ```
-
+### 2023-11-08 update
+modified the Dockerfile. Since Nvidia Jetson based on arm64, therefore the image should be built from ros's arm64 image.
 ### 2023-11-06 update
 
 automatically starts nodes in pkg fritzrobot_serial and fritzrobot_control.
--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-ROS_PATH="/home/jsy/catkin_ws"
-USER="jsy"
+ROS_PATH="/home/jetson/catkin_ws"
+USER="jetson"
 PASSWORD="jetson"
 echo ${PASSWORD} | sudo chmod 777 /dev/ttyACM0
 sudo chown ${USER}: -R /home/${USER}/
-source ${ROS_PATH}/devel/setup.bash
+source /opt/ros/noetic/setup.bash
 cd /home/${USER}/catkin_ws;
 catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3
 source ${ROS_PATH}/devel/setup.bash
