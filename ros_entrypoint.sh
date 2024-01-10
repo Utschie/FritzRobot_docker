aa86653330ca6ec20597d62483e9487cf75b39b6
Utschie <littlecat.j@hotmail.com>
2024-01-10 02:10:23 +0100

    installed more pkgs

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -4,10 +4,14 @@ USER="jetson"
 PASSWORD="jetson"
 echo ${PASSWORD} | sudo chmod 777 /dev/ttyACM0
 echo ${PASSWORD} | sudo chmod -R 777 /dev/input/
-echo ${PASSWORD} | chmod -R 777 ~/catkin_wk/src/ros_astra_camera
+echo ${PASSWORD} | chmod -R 777 ~/catkin_ws/src/ros_astra_camera
+sudo chmod -R 777 /dev/bus/usb
 sudo chown ${USER}: -R /home/${USER}/
+sudo chmod -R 777 /home/${USER}/catkin_ws/
 source /opt/ros/noetic/setup.bash
 cd /home/${USER}/catkin_ws;
 catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3
 source ${ROS_PATH}/devel/setup.bash
+cd cd ~/catkin_wk/src/ros_astra_camera/scripts
+./create_udev_rules
 roslaunch fritzrobot_control init.launch #需要前台运行一个进程，否则会container会自动退出
